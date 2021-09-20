3dff8bb7199aaac8f0aaf334d64afc6a4c80e1d3
Eugene Katrukha <katpyxa@gmail.com>
2021-09-20 16:43:41 +0200

    convolution multithreaded now

--- src/main/java/bigtrace/BigTrace.java
+++ src/main/java/bigtrace/BigTrace.java
@@ -560,28 +560,41 @@ public class BigTrace
 		ArrayImg<FloatType, FloatArray> hessFloat = ArrayImgs.floats( dim[ 0 ], dim[ 1 ], dim[ 2 ], 6 );
 		IntervalView<FloatType> hessian = Views.translate(hessFloat, nShift);
 		
-		ArrayImg<FloatType, FloatArray> dummyFloat = ArrayImgs.floats( dim[ 0 ], dim[ 1 ], dim[ 2 ]);
+		
 		//ArrayImg<FloatType, FloatArray> gradient = ArrayImgs.floats( dim[ 0 ], dim[ 1 ], dim[ 2 ], 3 );
+		
+		//long start1, end1;
+		
+		
 		int count = 0;
 		int [] nDerivOrder;
+		/**/
+		Convolution convObj;
+		//start1 = System.currentTimeMillis();
+		final int nThreads = Runtime.getRuntime().availableProcessors();
+		ExecutorService es = Executors.newFixedThreadPool( nThreads );
 
+		//second derivatives
 		for (int d1=0;d1<3;d1++)
 		{
 			for ( int d2 = d1; d2 < 3; d2++ )
 			{
-				final IntervalView< FloatType > hs2 = Views.hyperSlice( hessian, 3, count );
+				IntervalView< FloatType > hs2 = Views.hyperSlice( hessian, 3, count );
 				nDerivOrder = new int [3];
 				nDerivOrder[d1]++;
 				nDerivOrder[d2]++;
 				kernels = DerivConvolutionKernels.convolve_derive_kernel(sigma, nDerivOrder );
 				derivKernel = Kernel1D.centralAsymmetric(kernels);
-				SeparableKernelConvolution.convolution( derivKernel ).process( Views.extendBorder(input), hs2 );		
+				convObj=SeparableKernelConvolution.convolution( derivKernel );
+				convObj.setExecutor(es);
+				convObj.process(Views.extendBorder(input), hs2 );
 				//SeparableKernelConvolution.convolution( derivKernel ).process( input, hs2 );
 				count++;
 				System.out.println(count);
 			}
 		}
-
+		//end1 = System.currentTimeMillis();
+		//System.out.println("THREADED Elapsed Time in milli seconds: "+ (end1-start1));
 
 		EigenValVecSymmDecomposition<FloatType> mEV = new EigenValVecSymmDecomposition<FloatType>(3);
 		ArrayImg<FloatType, FloatArray> dV = ArrayImgs.floats( dim[ 0 ], dim[ 1 ], dim[ 2 ], 3 );
@@ -589,8 +602,8 @@ public class BigTrace
 		IntervalView<FloatType> directionVectors =  Views.translate(dV, nShift);
 		IntervalView<FloatType> salWeights =  Views.translate(sW, minV);
 	    
-		final int nThreads = Runtime.getRuntime().availableProcessors();
-		final ExecutorService es = Executors.newFixedThreadPool( nThreads );		 
+		//direction vectors and saliency weights for each voxel
+		 
 		mEV.computeRAI( hessian,directionVectors,salWeights,nThreads,es);
 		es.shutdown();
         
