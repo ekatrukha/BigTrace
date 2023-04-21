43bc5c79ef4ac3d5be31b98523063cf1d77b03ca
Eugene Katrukha <katpyxa@gmail.com>
2023-04-21 11:39:00 +0200

    minor

--- src/main/java/bigtrace/BigTraceRenderSettings.java
+++ src/main/java/bigtrace/BigTraceRenderSettings.java
@@ -11,28 +11,28 @@ import ij.Prefs;
 public class BigTraceRenderSettings implements Command {
 
 	@Parameter( label = "Render width" )
-	private int renderWidth = (int) Prefs.get("BigTrace.nRenderW",800.0);
+	public int renderWidth = (int) Prefs.get("BigTrace.nRenderW",800.0);
 
 	@Parameter( label = "Render height" )
-	private int renderHeight = (int) Prefs.get("BigTrace.nRenderH",600.0);
+	public int renderHeight = (int) Prefs.get("BigTrace.nRenderH",600.0);
 
 	@Parameter( label = "Dither window size",
 			choices = { "none (always render full resolution)", "2x2", "3x3", "4x4", "5x5", "6x6", "7x7", "8x8" } )
-	private String dithering = "3x3";
+	public String dithering = "3x3";
 
 	@Parameter( label = "Number of dither samples",
 			description = "Pixels are interpolated from this many nearest neighbors when dithering. This is not very expensive, it's fine to turn it up to 8.",
 			min="1",
 			max="8",
 			style="slider")
-	private int numDitherSamples = 8;
+	public int numDitherSamples = 8;
 
 	@Parameter( label = "GPU cache tile size" )
-	private int cacheBlockSize = 32;
+	public int cacheBlockSize = 32;
 
 	@Parameter( label = "GPU cache size (in MB)",
 				description = "The size of the GPU cache texture will match this as close as possible with the given tile size." )
-	private int maxCacheSizeInMB = 300;
+	public int maxCacheSizeInMB = 300;
 	@Override
 	public void run() {
 		
