c0ce4f4f196116daa7d5294747e67b0caef81bf0
Eugene Katrukha <katpyxa@gmail.com>
2024-06-26 16:05:08 +0200

    v.0.4.0

--- src/main/java/bigtrace/tracks/BigTraceTracksPanel.java
+++ src/main/java/bigtrace/tracks/BigTraceTracksPanel.java
@@ -9,6 +9,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -26,6 +29,7 @@ import javax.swing.event.ListSelectionListener;
 
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.LinAlgHelpers;
 
 import bigtrace.BigTrace;
 import bigtrace.BigTraceData;
@@ -33,7 +37,10 @@ import bigtrace.gui.GBCHelper;
 import bigtrace.gui.NumberField;
 import bigtrace.gui.PanelTitle;
 import bigtrace.gui.RangeSliderTF;
+import bigtrace.measure.MeasureValues;
+import bigtrace.rois.AbstractCurve3D;
 import bigtrace.rois.Roi3D;
+import ij.IJ;
 import ij.Prefs;
 
 
@@ -44,6 +51,7 @@ public class BigTraceTracksPanel < T extends RealType< T > & NativeType< T > > e
 
 	JButton butTrack;
 	JButton butSettings;
+	JButton butAlign;
 	JButton butDelete;
 	JButton butRename;
 	JButton butGroups;
@@ -138,11 +146,16 @@ public class BigTraceTracksPanel < T extends RealType< T > & NativeType< T > > e
 		cr.weighty=0.0;
 		cr.fill = GridBagConstraints.NONE;
 
-		butDelete = new JButton("Delete");
-		butDelete.addActionListener(this);
+		butAlign = new JButton("Align");
+		butAlign.addActionListener(this);
 		cr.gridx++;
 		cr.gridy++;
 		cr.gridheight=1;
+		panTracksChange.add(butAlign,cr);
+
+		butDelete = new JButton("Delete");
+		butDelete.addActionListener(this);
+		cr.gridy++;
 		panTracksChange.add(butDelete,cr);
 		
 		butRename = new JButton("Rename");
@@ -156,6 +169,8 @@ public class BigTraceTracksPanel < T extends RealType< T > & NativeType< T > > e
 		panTracksChange.add(butGroups,cr);
 
 		// a solution for now
+		butAlign.setMinimumSize(butRename.getPreferredSize());
+		butAlign.setPreferredSize(butRename.getPreferredSize());
 		butDelete.setMinimumSize(butRename.getPreferredSize());
 		butDelete.setPreferredSize(butRename.getPreferredSize());
 		butRename.setMinimumSize(butRename.getPreferredSize());		 
@@ -199,6 +214,111 @@ public class BigTraceTracksPanel < T extends RealType< T > & NativeType< T > > e
 		add(new JLabel(), cr);    
 	}
 	
+	void dialogAlign()
+	{
+		JPanel dialAlign = new JPanel(new GridBagLayout());
+		GridBagConstraints cd = new GridBagConstraints();
+		String [] sGroupSelection = new String [bt.roiManager.groups.size()];
+		for (int i = 0; i<bt.roiManager.groups.size(); i++)
+		{
+			sGroupSelection[i] = bt.roiManager.groups.get( i ).getName();
+		}
+		JComboBox<String> cbGroup = new JComboBox<>(sGroupSelection);
+		cd.gridx=0;
+		cd.gridy=0;
+		dialAlign.add(new JLabel("Group: "),cd);
+		cd.gridy++;
+		dialAlign.add(cbGroup,cd);
+		int reply = JOptionPane.showConfirmDialog(null, dialAlign, "Align trace ROIs in a group", 
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);		
+		
+		if (reply == JOptionPane.OK_OPTION) 
+		{
+			alignROIsInGroup(cbGroup.getSelectedIndex());
+		}
+	}
+	void alignROIsInGroup(final int indGroup)
+	{
+		class ROIsT
+		{
+			public final AbstractCurve3D sRoi;
+			public final int nTimeFrame;
+			public ROIsT(final AbstractCurve3D sRoi_, final int nTimeFrame_)
+			{
+				sRoi = sRoi_;
+				nTimeFrame = nTimeFrame_;
+			}
+		}
+		// creates the comparator for comparing stock value 
+		class ROIsTComparator implements Comparator<ROIsT> 
+		{ 
+		  
+		    @Override
+			public int compare(ROIsT s1, ROIsT s2) 
+		    { 
+		        if (s1.nTimeFrame == s2.nTimeFrame) 
+		            return 0; 
+		        else if (s1.nTimeFrame > s2.nTimeFrame) 
+		            return 1; 
+		        else
+		            return -1; 
+		    } 
+		} 
+		final ArrayList<ROIsT> groupROIs = new ArrayList<>();
+		Roi3D roi;
+		//fill the list
+		for(int i=0; i<bt.roiManager.rois.size();i++)
+		{
+			roi = bt.roiManager.rois.get( i );
+			if(roi.getGroupInd() == indGroup)
+			{
+				if(roi.getType()==Roi3D.LINE_TRACE || roi.getType()==Roi3D.POLYLINE)
+				{
+					groupROIs.add( new ROIsT(( AbstractCurve3D ) roi,roi.getTimePoint()) );
+				}
+			}
+		}
+		if(groupROIs.size()<1)
+		{
+			IJ.log( "Cannot find traces to align in this group." );
+			return;
+		}
+		
+		Collections.sort(groupROIs, new ROIsTComparator()); 
+		
+		MeasureValues oldVect = new MeasureValues();
+		MeasureValues newVect = new MeasureValues();
+		int nFrameN = groupROIs.get( 0 ).nTimeFrame;
+		(groupROIs.get( 0 ).sRoi).getEndsDirection(oldVect, BigTraceData.globCal);
+		for(int i = 1; i<groupROIs.size();i++)
+		{
+			AbstractCurve3D newRoi = groupROIs.get( i ).sRoi;
+			if(nFrameN - newRoi.getTimePoint() == 0)
+			{
+				IJ.log("There are multiple ROIs in the same frame number #" +Integer.toString( nFrameN ));
+			}
+			nFrameN = newRoi.getTimePoint();
+			newRoi.getEndsDirection(newVect, BigTraceData.globCal);
+			//if not looking at the same direction
+			if(LinAlgHelpers.dot( oldVect.direction.positionAsDoubleArray(),  newVect.direction.positionAsDoubleArray())<0)
+			{
+				newRoi.reversePoints();
+				for(int d=0;d<3;d++)
+				{
+					newVect.direction.setPosition((-1)*newVect.direction.getDoublePosition( d ) , d );
+				}
+				//System.out.println("swapped");
+
+			}
+			for(int d=0;d<3;d++)
+			{
+				oldVect.direction.setPosition(newVect.direction.getDoublePosition( d ) , d );
+			}
+		}
+		IJ.log( "Aligned "+Integer.toString( groupROIs.size() )+ " ROIs in the " +bt.roiManager.groups.get( indGroup ).getName()+" group.");
+		
+	}
+	/** tracking of LineTrace over time**/
 	void simpleTracking()
 	{
 		JPanel dialogTrackSettings = new JPanel();
@@ -321,7 +441,11 @@ public class BigTraceTracksPanel < T extends RealType< T > & NativeType< T > > e
 			bt.roiManager.showGroupsDialog();
 			
 		}
-		
+		//ALIGN ROIs in a GROUP
+		if(e.getSource() == butAlign)
+		{
+			dialogAlign();
+		}
 		//DELETE
 		if(e.getSource() == butDelete)
 		{
